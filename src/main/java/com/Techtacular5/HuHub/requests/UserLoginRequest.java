dc533f22c5bd3ba952cb57a55954a3417c9722fb
b21946261 <b21946261@cs.hacettepe.edu.tr>
2023-06-01 21:42:47 +0300

    d

--- src/main/java/com/Techtacular5/HuHub/requests/UserLoginRequest.java
+++ src/main/java/com/Techtacular5/HuHub/requests/UserLoginRequest.java
@@ -1,2 +1,9 @@
-package com.Techtacular5.HuHub.requests;public class UserLoginRequest {
+package com.Techtacular5.HuHub.requests;
+
+import lombok.Data;
+
+@Data
+public class UserLoginRequest {
+    String email;
+    String password;
 }
